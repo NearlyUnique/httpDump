baa1933aab147e2558b161d681bf30ff3174ac64
Adam Straughan <adam.g.straughan@uk.tesco.com>
2018-02-07 15:19:02 +0000

    Add curl output support

--- main.go
+++ main.go
@@ -1,33 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/fatih/color"
 )
 
-const (
-	bright  = "\033[1m"
-	clear   = "\033[0m"
-	black   = 30
-	red     = 31
-	green   = 32
-	yellow  = 33
-	blue    = 34
-	magenta = 35
-	cyan    = 36
-	white   = 37
+var (
+	blue      = color.New(color.FgBlue).SprintFunc()
+	yellow    = color.New(color.FgYellow).SprintFunc()
+	green     = color.New(color.FgGreen).SprintFunc()
+	cyan      = color.New(color.FgCyan).SprintFunc()
+	magentaBg = color.New(color.BgMagenta).SprintFunc()
+	magentaFg = color.New(color.FgMagenta).SprintFunc()
 )
-
-func colour(c int, text string) string {
-	return fmt.Sprintf("\033[%dm%s%s", c, text, clear)
-}
-func colourB(c int, text string) string {
-	return fmt.Sprintf("\033[%dm%s%s%s", c, bright, text, clear)
-}
 
 func mapToStringSlice(headers map[string][]string) string {
 	s := ""
@@ -38,23 +30,54 @@ func mapToStringSlice(headers map[string][]string) string {
 }
 
 func main() {
+	pretty := flag.Bool("pretty", true, "When true pretty print")
+	useColor := flag.Bool("color", true, "When true use coloured output")
+	curl := flag.Bool("curl", false, "When true report in curl format")
+	flag.Parse()
+
 	port := os.Getenv("PORT")
+
+	color.NoColor = !*useColor
 
 	if len(port) == 0 {
 		port = "9000"
 	}
 	addr := ":" + port
-	fmt.Printf("listening on %s\n", colour(blue, addr))
+	printf("listening on %s\n", blue(addr))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		buf, _ := ioutil.ReadAll(r.Body)
-		fmt.Printf("-------------------\nrequest:\n%s %s\n\nheaders:\n%v\nbody:\n%s\n",
-			colour(yellow, r.Method),
-			colour(green, r.URL.String()),
-			colourB(magenta, mapToStringSlice(r.Header)),
-			colour(cyan, string(buf)),
-		)
+		body := string(buf)
+		if *pretty {
+			printf("-------------------\nrequest:\n%s %s\n\nheaders:\n%v\nbody:\n%s\n",
+				yellow(r.Method),
+				green(r.URL.String()),
+				magentaBg(mapToStringSlice(r.Header)),
+				cyan(body),
+			)
+		}
+		if *curl {
+			printCurl(r, body)
+		}
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintf(w, "{\"time\":\"%d\"}", time.Now().Unix())
 	})
 	http.ListenAndServe(addr, nil)
+}
+
+func printCurl(r *http.Request, body string) {
+	printf(blue("curl"))
+	printf("   -X %s \\\n", yellow(r.Method))
+	for k, v := range r.Header {
+		for _, vi := range v {
+			printf("  -H \"%s: %s\" \\\n", magentaBg(k), magentaFg(vi))
+		}
+	}
+	if len(body) > 0 {
+		printf("   -d '%s' \\\n", body)
+	}
+	printf("   \"https://%s%s\" \n", r.Host, green(r.URL.String()))
+}
+
+func printf(format string, a ...interface{}) (n int, err error) {
+	return fmt.Fprintf(color.Output, format, a...)
 }
